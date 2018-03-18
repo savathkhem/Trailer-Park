7ac7fd302efb8e039a6fd6b1db938d356a40a31b
Unknown <savath.khem@live.com>
2018-03-18 17:02:34 -0400

    CSS cleanup, staging area for poster div

--- assets/javascript/Trailer-Park.js
+++ assets/javascript/Trailer-Park.js
@@ -1,6 +1,4 @@
-$('#modal1').modal('open');
-$('#modal1').modal('close');
- 
+
 $('.modal').modal({
     dismissible: true, // Modal can be dismissed by clicking outside of the modal
     opacity: .5, // Opacity of modal background
@@ -9,9 +7,9 @@ $('.modal').modal({
     startingTop: '4%', // Starting top style attribute
     endingTop: '10%', // Ending top style attribute
     ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-      alert("Ready");
+    //   alert("Ready");
       console.log(modal, trigger);
     },
-    complete: function() { alert('Closed'); } // Callback for Modal close
+    complete: function() {} // Callback for Modal close
   }
 );
