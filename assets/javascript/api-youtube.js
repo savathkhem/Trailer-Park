40458bf2e129a3b1891516db51c5fc279d205db7
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-03-20 11:59:29 -0400

    click event now populates videos called from youtube api to screen.

--- assets/javascript/api-youtube.js
+++ assets/javascript/api-youtube.js
@@ -3,13 +3,24 @@ $(document).ready(function(){
 
     var movieTrailersId = 'UC3gNmTGu-TTbFPpfSs5kNkg';
 
+    var testPlaylistId = 'PLBCF2DAC6FFB574DE'
+
+
     $("#test1").on("click",function(){
       console.log('test1 clicked!')
       $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails&maxResults=10&id="+movieTrailersId+"&key="+apiKey,
+        url: "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=10&playlistId="+testPlaylistId+"&key="+apiKey,
         type: "GET",
       }).then(function(response) {
         console.log(response);
+        console.log(response.items)
+        for (i = 0; i < response.items.length; i++){
+          console.log(response.items[i])
+          var videoId = response.items[i].contentDetails.videoId
+          var baseUrl = 'https://www.youtube.com/embed/'
+          var embedVideo = `<iframe id="ytplayer" type="text/html" width="640" height="360"src="${baseUrl + videoId}"></iframe>`;
+          $("#videos-display").append(embedVideo)
+        }
       });
     })
 
