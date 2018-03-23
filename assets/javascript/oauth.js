93ad57cccdeaf986bd221b1940be7b327ebc84c1
Unknown <savath.khem@live.com>
2018-03-23 00:24:23 -0400

    ADDED Firebase, Oauth, Google Cloud API

---
+++ assets/javascript/oauth.js
@@ -0,0 +1,11 @@
+const hintPromise = googleyolo.hint({
+    supportedAuthMethods: [
+      "https://accounts.google.com"
+    ],
+    supportedIdTokenProviders: [
+      {
+        uri: "https://accounts.google.com",
+        clientId: "658789466925-63l1686k250p6mk1et5rs6vp81b3a0pk.apps.googleusercontent.com"
+      }
+    ]
+  });
