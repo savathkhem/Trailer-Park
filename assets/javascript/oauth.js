e5c38a3128d918bc2bb4176aa226409284138ffe
Unknown <savath.khem@live.com>
2018-03-24 13:07:42 -0400

    ADDED OAUTH

--- assets/javascript/oauth.js
+++ assets/javascript/oauth.js
@@ -1,95 +1,52 @@
-var config = {
-  apiKey: "AIzaSyCHDk62ibv6RkEW0AP2CJdxPMTdQocgnCk",
-  authDomain: "trailerpark-trai-1521756731455.firebaseapp.com",
-  databaseURL: "https://trailerpark-trai-1521756731455.firebaseio.com",
-  projectId: "trailerpark-trai-1521756731455",
-  storageBucket: "trailerpark-trai-1521756731455.appspot.com",
-  messagingSenderId: "184928832461"
-};
-firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCHDk62ibv6RkEW0AP2CJdxPMTdQocgnCk",
+      authDomain: "trailerpark-trai-1521756731455.firebaseapp.com",
+      databaseURL: "https://trailerpark-trai-1521756731455.firebaseio.com",
+      projectId: "trailerpark-trai-1521756731455",
+      storageBucket: "trailerpark-trai-1521756731455.appspot.com",
+      messagingSenderId: "184928832461"
+    };
+    firebase.initializeApp(config);
 
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
 
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    // List of OAuth providers supported.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  var uiConfig = {
+      callbacks: {
+        signInSuccess: function(currentUser, credential, redirectUrl) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      signInSuccessUrl: 'index.html',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>'
+    };
 
-var uiConfig = {
-  callbacks: {
-    signInSuccess: function(currentUser, credential, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>'
-};
-
-// The start method will wait until the DOM is loaded.
+    // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+  
+  
 
 
-
-// const hintPromise = googleyolo.hint({
-//     supportedAuthMethods: [
-//       "https://accounts.google.com"
-//     ],
-//     supportedIdTokenProviders: [
-//       {
-//         uri: "https://accounts.google.com",
-//         clientId: "658789466925-63l1686k250p6mk1et5rs6vp81b3a0pk.apps.googleusercontent.com"
-//       }
-//     ]
-//   });
-
-
-  // function onSignIn(googleUser) {
-  //   var profile = googleUser.getBasicProfile();
-  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //   console.log('Name: ' + profile.getName());
-  //   console.log('Image URL: ' + profile.getImageUrl());
-  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  // }
-
-  // function signOut() {
-  //   var auth2 = gapi.auth2.getAuthInstance();
-  //   auth2.signOut().then(function () {
-  //     console.log('User signed out.');
-  //   });
-  // }
+    
